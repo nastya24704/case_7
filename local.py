b94947a7fbdb2814a1ca0c9c4c30fd0c5e0e8177
nastya24704 <nastazeravina@gmail.com>
2025-09-30 23:22:56 +0700

    Create local.py

---
+++ local.py
@@ -0,0 +1,22 @@
+NAME = '''Наименование
+'''
+COUNTRY = '''Страна
+'''
+ARTICLE = '''Артикул
+'''
+COLOR = '''Цвет
+'''
+TYPE_OF_SHOE = '''Вид обуви
+'''
+UPPER_MATERIAL = '''верхний материал
+'''
+SIZE = '''Размер
+'''
+SEASON = '''Сезон
+'''
+PRICE = '''Цена
+'''
+ENTER_SEARCH_QUERY = '''Введите поисковый запрос
+'''
+PRODUCT_INFORMATION_IN_THE_FOLLOWING_FILE = '''Информация о товаре в следующем файле
+'''
